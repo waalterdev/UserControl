0680ad4d2ea23b1c601cf3dff7ff6ed6de47c483
Sherman Walter <srzprivate@outlook.com>
2025-02-21 11:19:57 -0300

    initial commit

---
+++ src/main/java/org/userControl/User.java
@@ -0,0 +1,12 @@
+package org.userControl;
+
+
+public class UserModel {
+    private String firstname;
+    private String lastname;
+
+    public UserModel(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+}
---
+++ src/main/java/org/userControl/UserManagement.java
@@ -0,0 +1,4 @@
+package org.userControl;
+
+public class UserManagement {
+}
